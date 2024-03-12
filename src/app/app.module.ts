d5f58c1f45b3f98dfbb95dfb6121c0e50e832418
Luc-Siat <lucsiat@gmail.com>
2024-03-12 16:32:25 +0100

    add base components and utils to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectCardComponent } from './components/project-list/project-card/project-card.component';
+import { ProjectListComponent } from './components/project-list/project-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ProjectCardComponent,
+    ProjectListComponent
   ],
   imports: [
     BrowserModule
