ebc1d26866d927fd3df189de851c7714f622ad3f
Luc-Siat <lucsiat@gmail.com>
2024-04-23 19:42:37 +0200

    add a style.css to the docs folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}",
-  "../src/**/*.{html,ts}"],
+  content: 
+  ["./src/**/*.{html,ts}",
+  "../src/**/*.{html,ts}",
+  "../../src/**/*.{html,ts}"
+  ],
   theme: {
     extend: {
         container: {
